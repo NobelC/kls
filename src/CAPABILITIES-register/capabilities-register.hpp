f2c84d71ebe98c8781461dc0b1c5a5519d10cede
NobelC <jeremynobel16@gmail.com>
2026-08-13 17:37:28 -0500

    refactor: remove legacy register directories
    - Delete src/CAPABILITIES-register (replaced by finding_registry)
    - Delete src/SUID-SGID-register (replaced by finding_registry)
    - Delete src/white-list-routes (replaced by whitelist.hpp)
    
    All functionality now provided by constexpr headers.

--- src/CAPABILITIES-register/capabilities-register.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "kls/findings/finding_flags.hpp"
-#include "kls/detail/Id.hpp"
-
-void GeneralCapabilityLog(const kls::findings::Finding& health);
-void PrintCapabilityFlags(const ID& id);
-const kls::findings::Finding* GetCapabilityFlag(const ID& id);
-void CreatedCapabilityFlags();
-
