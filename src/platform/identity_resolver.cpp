564c2c74178b40889f1866a8e84aa3ac0f8ad6bd
NobelC <jeremynobel16@gmail.com>
2026-08-15 22:17:58 -0500

    fix(platform): restore correct owner/group name resolution
    - Fix bug where owners_name and groups_name always returned 'UNKNOWN'
    - Restore assignment of result_owner->pw_name and result_group->gr_name
    - Bug was introduced when removing redundant null checks in previous commit

--- src/platform/identity_resolver.cpp
+++ src/platform/identity_resolver.cpp
@@ -28,30 +28,28 @@ namespace kls::platform{
     if(s_err != 0){
       throw std::system_error(s_err, std::generic_category(), "fail in getpwuid_r");
     }
-    
+
     if(result_owner == nullptr){
       owners_[uid] = "UNKNOWN";
       return owners_[uid];
     }
 
-    owners_[uid] = "UNKNOWN";
+    owners_[uid] = result_owner->pw_name;
     return owners_[uid];
-
   };
-
+  
   [[nodiscard]] std::string_view IdentityResolver::groups_name(gid_t gid){
     auto it = groups_.find(gid);
     if(it != groups_.end()){
       return it->second;
     }
-        
-
 
     struct group gp;
     struct group* result_group = nullptr;
-    
+
     long s = sysconf(_SC_GETGR_R_SIZE_MAX);
     size_t bufflen = (s == -1) ? 16384 : static_cast<size_t>(s);
+
     std::vector<char> buffer(bufflen);
 
     int s_err;
@@ -60,16 +58,15 @@ namespace kls::platform{
     }
 
     if (s_err != 0) {
-        throw std::system_error(s_err, std::generic_category(), "fail in  getgrgid_r");
+      throw std::system_error(s_err, std::generic_category(), "fail in  getgrgid_r");
     }
 
     if (result_group == nullptr) {
         groups_[gid] = "UNKNOWN";
         return groups_[gid];
     }
-
-    groups_[gid] = "UNKNOWN";
+    
+    groups_[gid] = result_group->gr_name;
     return groups_[gid];
-
   };
 }
